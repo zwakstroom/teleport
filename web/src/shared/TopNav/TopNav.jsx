f571d23c1f8c1044d37da027253b32f8f14ebd29
Alex Wolfe <thewolfe@gmail.com>
2018-12-06 12:32:32 -0800

    top nav base

---
+++ web/src/shared/TopNav/TopNav.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+class TopNav extends React.Component {
+  constructor(props) {
+    super(props);
+
+
+  }
+
+  render() {
+    return (
+      <div>Hello World</div>
+    );
+  }
+}
+
+export default TopNav;
