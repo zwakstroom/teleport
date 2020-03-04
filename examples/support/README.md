166bec1504b21ab1f92203d6f5c71ace4cdf4b03
Sasha Klizhentas <klizhentas@gmail.com>
2020-03-03 21:08:08 -0800

    Initial support agent implementation

---
+++ examples/support/README.md
@@ -0,0 +1,9 @@
+# Intro
+
+This is a sample support script featuring IOT capabilities of teleport,
+it creates a throwaway node running in a foreground and granting
+access to remote infrastructure for debugging purposes:
+
+```bash
+curl https://s3-us-west-2.amazonaws.com/dev.gravitational.io/support.sh | sudo bash -s <token>
+```
---
+++ examples/support/support.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+set -euo pipefail
+
+TELEPORT_VERSION="4.2.3"
+TARGET="iot.gravitational.io:443"
+
+if [ "$#" -ge 2 ]; then
+    TOKEN=$1
+    CUSTOMER=$2
+else
+  echo -e "Enter customer name and join token: ./support.sh <token> <customer>";
+  exit 1;
+fi
+
+[ -n "$1" ] && TOKEN=$1 || { echo -n "Enter join token: "; read TOKEN; }
+[ -n "$2" ] && CUSTOMER=$2 || { echo -n "Enter customer name: "; read CUSTOMER; }
+
+for TOOL in curl tar
+do
+    command -v $TOOL >/dev/null 2>&1 || { echo >&2 "I require $TOOL but it's not installed. Aborting."; exit 1; }
+done
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    ARCH="linux-amd64"
+    echo -e "Starting Teleport support agent on Linux..."
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    ARCH="darwin-amd64"
+    echo -e "Starting Teleport support agent on Mac OS..."
+else
+    echo -e "Unsupported platform $OSTYPE"
+    exit 1
+fi
+
+TEMP_DIR=$(mktemp -d -t teleport-XXXXXXXXXX)
+echo -e "Creating temp dir $TEMP_DIR"
+
+finish()
+{
+    echo -e "Deleting temp dir $TEMP_DIR"
+    echo -e "Exiting...\n";
+    popd
+    exit 0;
+}
+
+trap finish EXIT
+
+pushd $TEMP_DIR
+URL="https://get.gravitational.com/teleport-v${TELEPORT_VERSION}-$ARCH-bin.tar.gz"
+echo -e "Downloading official teleport distribution from $URL"
+curl -L -s $URL | tar xz -C $TEMP_DIR
+cat > $TEMP_DIR/teleport.yaml <<- EOM
+teleport:
+  log:
+    output: stdout
+    severity: ERROR
+  auth_token: "${TOKEN}"
+  data_dir: "${TEMP_DIR}"
+  auth_servers: ["${TARGET}"]
+  storage:
+    type: dir
+    path: "${TEMP_DIR}"
+labels:
+        role: master
+        type: postgres
+ssh_service:
+  enabled: yes
+  labels:
+    role: support
+    customer: "${CUSTOMER}"
+  commands:
+  - name: host
+    command: ['/bin/hostname']
+    period: 1h0m0s
+auth_service:
+  enabled: no
+proxy_service:
+  enabled: no
+EOM
+echo -e "Starting up support agent in foreground. Press Ctrl-C to exit."
+./teleport/teleport start --config=$TEMP_DIR/teleport.yaml
