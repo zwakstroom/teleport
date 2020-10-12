8afdecc484cbb73ba95d1a2bf060d8f4af046989
Quin Quintero <quinq@gravitational.com>
2020-10-12 12:58:48 -0700

    test with step not being even multiple

--- lib/benchmark/linear_test.go
+++ lib/benchmark/linear_test.go
@@ -21,11 +21,15 @@ func TestGenerate(t *testing.T) {
 		MinimumMeasurments: 0,
 	}
 
-	linearConfig := Linear{LowerBound: 10,
-		UpperBound: 50, Step: 10,
-		MinimumMeasurment: 1000, MinimumWindow: d, config: initial,
+	linearConfig := Linear{
+		LowerBound:        10,
+		UpperBound:        50,
+		Step:              10,
+		MinimumMeasurment: 1000,
+		MinimumWindow:     d,
+		config:            initial,
 	}
-
+	// First generation
 	res := linearConfig.Generate()
 	if res != true {
 		t.Errorf("failed to generate first generation")
@@ -38,7 +42,7 @@ func TestGenerate(t *testing.T) {
 	expected := initial
 	expected.Rate = 10
 	assert.Empty(t, cmp.Diff(expected, bm))
-	//------
+	// Second generation
 	res = linearConfig.Generate()
 	if res != true {
 		t.Errorf("failed to generate #2 generation")
@@ -52,7 +56,7 @@ func TestGenerate(t *testing.T) {
 	expected.Rate = 20
 	assert.Empty(t, cmp.Diff(expected, bm))
 
-	//--------
+	// Third generation
 	res = linearConfig.Generate()
 	if res != true {
 		t.Errorf("failed to generate #3 generation")
@@ -64,7 +68,8 @@ func TestGenerate(t *testing.T) {
 	}
 	expected.Rate = 30
 	assert.Empty(t, cmp.Diff(expected, bm))
-	//-----
+
+	// Fourth generation
 	res = linearConfig.Generate()
 	if res != true {
 		t.Errorf("failed to generate #4 generation")
@@ -77,8 +82,7 @@ func TestGenerate(t *testing.T) {
 	expected.Rate = 40
 	assert.Empty(t, cmp.Diff(expected, bm))
 
-	//---
-
+	// Fifth generation
 	res = linearConfig.Generate()
 	if res != true {
 		t.Errorf("failed to generate #5 generation")
@@ -91,8 +95,7 @@ func TestGenerate(t *testing.T) {
 	expected.Rate = 50
 	assert.Empty(t, cmp.Diff(expected, bm))
 
-
-	//---
+	// Sixth generation, should return false
 	res = linearConfig.Generate()
 	if res != false {
 		t.Errorf("expected false, got true")
@@ -102,10 +105,65 @@ func TestGenerate(t *testing.T) {
 	if err == nil {
 		t.Errorf("generating more benchmarks than expected")
 	}
+}
 
+func TestGenerateNotEvenMultiple(t *testing.T) {
 
-	// min == max
-	// not an even multiple
-	// an even multiple
+	d, _ := time.ParseDuration("30s")
 
+	initial := Config{
+		Threads:            10,
+		Rate:               0,
+		Command:            []string{"ls"},
+		Interactive:        false,
+		MinimumWindow:      d,
+		MinimumMeasurments: 0,
+	}
+
+	linearConfig := Linear{
+		LowerBound:        10,
+		UpperBound:        20,
+		Step:              7,
+		MinimumMeasurment: 1000,
+		MinimumWindow:     d,
+		config:            initial,
+	}
+	expected := initial
+	res := linearConfig.Generate()
+	if res != true {
+		t.Errorf("failed to generate first generation")
+	}
+
+	_, bm, err := linearConfig.GetBenchmark()
+	if err != nil {
+		t.Errorf("failed to get current benchmark")
+	}
+	expected.Rate = 10
+	assert.Empty(t, cmp.Diff(expected, bm))
+
+	// Second generation, rate = 17
+	res = linearConfig.Generate()
+	if res != true {
+		t.Errorf("failed to generate first generation")
+	}
+
+	_, bm, err = linearConfig.GetBenchmark()
+	if err != nil {
+		t.Errorf("failed to get current benchmark")
+	}
+	expected.Rate = 17
+	assert.Empty(t, cmp.Diff(expected, bm))
+
+	// Third generation, rate > upper bound
+	res = linearConfig.Generate()
+	if res != false {
+		t.Errorf("expected false, exceeded upper bound")
+	}
+
+	_, _, err = linearConfig.GetBenchmark()
+	if err == nil {
+		t.Errorf("generating more benchmarks than expected")
+	}
+	expected.Rate = 17
+	assert.Empty(t, cmp.Diff(expected, bm))
 }
